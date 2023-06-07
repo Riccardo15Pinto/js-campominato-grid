11b504d70b7a6787715b282e3899b00423ff062e
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-07 13:56:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//check console log
+console.log('ok js');
+
+const selectElement = document.getElementById('select');
+const buttonElement = document.getElementById('button');
+const containerElement = document.querySelector('.container');
+
+
+
+function createcell(){
+    const cell = document.createElement('div')
+
+    return cell;
+}
