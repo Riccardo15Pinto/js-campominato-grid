8c648e07f9c6a92fa16d5a9a7a1a33de4fdc0fe9
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-07 14:05:39 +0200

    take element from DOM and create function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 //check console log
 console.log('ok js');
 
+//recupero elementi dal DOM
 const selectElement = document.getElementById('select');
 const buttonElement = document.getElementById('button');
 const containerElement = document.querySelector('.container');
 
-
-
-function createcell(){
+//creo una funzione che generi delle celle
+function createcell(a){
     const cell = document.createElement('div')
-
+    cell.className = a;
     return cell;
 }
