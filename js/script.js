48c203091c9bee88f5c57d3efcc75aebc4fc6f2f
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-07 16:57:39 +0200

    fix button (addeventlistner)

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ console.log('ok js');
 //recupero elementi dal DOM
 const selectElement = document.getElementById('select');
 const buttonElement = document.getElementById('button');
+const deleteElement = document.getElementById('delete');
 const containerElement = document.querySelector('.container');
 
 //creo una funzione che generi delle celle
-function createcell(a){
+function createcell(a , b){
     const cell = document.createElement('div')
-    cell.className = a;
+    cell.classList.add(a);
+    cell.innerText = b;
     return cell;
 }
 
@@ -18,13 +20,14 @@ const cols = 10;
 let total = row * cols;
 console.log(total);
 let gameSelect = 'easy';
-let cell;
+let cells;
+
+
 
 buttonElement.addEventListener('click' , function(){
     //recupero valore select
     let userChoice = parseInt(selectElement.value);
     console.log(userChoice);
-    
     if(userChoice === 2){
         total = (row * cols) - 19;
         gameSelect = 'medium';
@@ -34,11 +37,21 @@ buttonElement.addEventListener('click' , function(){
     }
     
     for(let i = 1; i <= total; i++){
-        cell = createcell(gameSelect);
-        cell.append([i]);
-        containerElement.appendChild(cell);
+        cells = createcell(gameSelect , i);
+        cells.addEventListener('click' , () => {
+            cells.classList.add('b-gblue');
+            console.log(cells[i]);
+        });
+        containerElement.appendChild(cells);
 
     }
     
+
     
 });
+
+
+
+
+
+
